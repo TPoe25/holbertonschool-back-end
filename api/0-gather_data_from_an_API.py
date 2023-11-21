200962aef8d15eda183dfb2d46c8ee198c46f08d
TPoe25 <t_poe25@yahoo.com>
2023-11-21 16:52:16 -0600

    checking

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,32 +3,25 @@
 Gathers data fron APT and shows TODO list for employee ID
 """
 from sys import argv
+import json 
 import requests
 
+
 if __name__ == '__main__':
-    # Checks if number of args is provided
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
-        exit(1)
-        
-    employee_id = argv[1]
-    base_url = "http://jsonplaceholder.typicode.com"
-    
-    # User Info
-    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
-    user_data = user_response.json()
-    employee_name = user_data.get("username")
-    
-    # User's TODO list
-    todo_response = requests.get("{}/todos?userId={}".format(base_url, employee_id))
-    todo_data = todo_response.json()
-    
-    # Calculate the TODO progress
-    total_tasks =  len(todo_data)
-    completed_tasks = sum(task.get("completed") for task in todo_data)
-    
-    # Shows the list progress
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for task in todo_data:
-        if task.get("completed"):
-            print(f"\t{task.get('title')}")
+    url = 'https://jsonplaceholder.typicode.com/users'
+    response = requests.get(url)
+    users = response.json()
+
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(url)
+    todos = response.json()
+
+    user_dict = {}
+    for user in users:
+        user_id = str(user.get('id'))
+        username = user.get('username')
+        tasks = [{"username": username, "task": task.get('title'), "completed": task.get('completed')} for task in todos if task.get('userId') == user.get('id')]
+        user_dict[user_id] = tasks
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(user_dict, jsonfile)
