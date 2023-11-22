9fdc48dba81ffb09f0c7c243bb12d0ac8f4c478e
TPoe25 <t_poe25@yahoo.com>
2023-11-21 20:47:44 -0600

    commit

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
 """
-Gathers data from API and shows TODO list for employee ID
+Gathers data fron APT and shows TODO list for employee ID
 """
 from sys import argv
 import requests
 import json
-
-def get_user_info(employee_id):
-    base_url = "http://jsonplaceholder.typicode.com"
-    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
-
-    # Check if the user ID exists
-    if user_response.status_code != 200:
-        print("User with ID {} not found.".format(employee_id))
-        exit(1)
-
-    user_data = user_response.json()
-    employee_name = user_data.get("username")
-    return employee_name
 
 if __name__ == '__main__':
     # Checks number of args provided
@@ -26,12 +13,15 @@ if __name__ == '__main__':
         exit(1)
 
     employee_id = argv[1]
+    base_url = "http://jsonplaceholder.typicode.com"
 
-    # Get user info
-    employee_name = get_user_info(employee_id)
+    # Users info
+    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
+    user_data = user_response.json()
+    employee_name = user_data.get("username")
 
     # User's TODO list
-    todo_response = requests.get("http://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id))
+    todo_response = requests.get("{}/todos?userId={}".format(base_url, employee_id))
     todo_data = todo_response.json()
 
     # Prepares the data in JSON layout
